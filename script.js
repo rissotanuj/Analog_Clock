555c90726152117b60e2592f889d083faed992de
rissotanuj <risso.tanuj@gmail.com>
2023-01-26 01:18:27 +0530

    first commit of clock applicaton

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+setInterval(() => {
+
+    let hour = document.getElementById("hour");
+    let minute = document.getElementById("minute");
+    let second = document.getElementById("second");
+
+    let date = new Date();
+    let thour = date.getHours();
+    let tminute = date.getMinutes();
+    let tsecond = date.getSeconds();
+
+    let hrotation = thour*30+tminute/2;
+    let trotation = tminute*6;
+    let srotation = tsecond*6;   
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${trotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+
+}, 1000);
+   
